5ce84a5cf5002c66e0e19a437852ec377af2af29
Bob Follek <bfollek@gmail.com>
2019-06-30 17:45:12 -0400

    WIP signing_key is correct

--- test_oauth_builder.py
+++ test_oauth_builder.py
@@ -10,9 +10,9 @@ REQUEST_PARAMS = {
     "status": "Hello Ladies + Gentlemen, a signed OAuth request!",
 }
 CONSUMER_KEY = "xvz1evFS4wEEPTGEFPHBog"
-CONSUMER_SECRET = ""
+CONSUMER_SECRET = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"
 ACCESS_TOKEN = "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"
-ACCESS_TOKEN_SECRET = ""
+ACCESS_TOKEN_SECRET = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
 SIGNATURE = "tnnArxj06cWHq44gCs1OSKk/jLY="
 NONCE = "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"
 TIMESTAMP = "1318622958"
@@ -63,4 +63,12 @@ def test_sig_base_string():
     assert (
         s
         == "POST&https%3A%2F%2Fapi.twitter.com%2F1.1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521"
+    )
+
+
+def test_signing_key():
+    oa = from_known_values()
+    assert (
+        oa._signing_key(CONSUMER_SECRET, ACCESS_TOKEN_SECRET)
+        == "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
     )
