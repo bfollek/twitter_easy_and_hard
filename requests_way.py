812ea827408ca65d810c4d2ea84a1ad615b27738
Bob Follek <bfollek@gmail.com>
2019-06-30 21:26:13 -0400

    requests_way working 2 ways

--- requests_way.py
+++ requests_way.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-import request
+import requests
+from requests_oauthlib import OAuth1
 
+from oauth_builder import OauthBuilder
 from random_string import RandomString
 
 
@@ -11,11 +13,31 @@ def send_tweet(status):
     consumer_secret = os.environ["TWITTER_TEST_CONSUMER_SECRET"]
     access_token = os.environ["TWITTER_TEST_ACCESS_TOKEN"]
     access_token_secret = os.environ["TWITTER_TEST_ACCESS_TOKEN_SECRET"]
-    # try:
-    #     response = twitter.update_status(status=status)
-    #     #response.status_code
-    #     return None
-    return "TBD"
+    auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    r = requests.post(url, data={"status": status}, auth=auth)
+    return r.status_code
+
+
+def send_tweet2(status):
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    request_params = {"status": status}
+    consumer_key = os.environ["TWITTER_TEST_CONSUMER_KEY"]
+    consumer_secret = os.environ["TWITTER_TEST_CONSUMER_SECRET"]
+    access_token = os.environ["TWITTER_TEST_ACCESS_TOKEN"]
+    access_token_secret = os.environ["TWITTER_TEST_ACCESS_TOKEN_SECRET"]
+    oa = OauthBuilder(
+        "post",
+        url,
+        request_params,
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret,
+    )
+    headers = {"authorization": oa.authorization_header()}
+    r = requests.post(url, data=request_params, headers=headers)
+    return r.status_code
 
 
 if __name__ == "__main__":
@@ -24,10 +46,17 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         status = f"{sys.argv[1]} {RandomString.make()} {RandomString.make()}"
         print(f"Tweeting {status}")
-        s = send_tweet(status)
-        if s:
-            print(s)
-        else:
+        rv = send_tweet(status)
+        if rv == 200:
             print("Success!")
+        else:
+            print(f"Bad status: {rv}")
+        status = f"{sys.argv[1]} {RandomString.make()} {RandomString.make()}"
+        print(f"Tweeting {status}")
+        rv = send_tweet2(status)
+        if rv == 200:
+            print("Success!")
+        else:
+            print(f"Bad status: {rv}")
     else:
-        print("You forgot the status!")
+        print("You forgot the tweet!")
